0d80bdcc03152e851ae795532122cce8ffcdc10e
Hiroyuki Aoki <dsch1975@gmail.com>
2016-03-23 10:18:25 +0900

    Initial Commit

---
+++ NoteApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NoteApp
+//
+//  Created by Hiroyuki Aoki on 2016/03/23.
+//  Copyright © 2016年 sample. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
