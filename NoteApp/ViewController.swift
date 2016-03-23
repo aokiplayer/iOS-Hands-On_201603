09ddabf522c07a65e083b5e3563abda280dfd673
Hiroyuki Aoki <aoki@casareal.co.jp>
2016-03-23 10:33:08 +0900

    ビューの接続済み
    テキストフィールドとテキストビューはアウトレット接続、ボタンはアクション接続。

--- NoteApp/ViewController.swift
+++ NoteApp/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var textView: UITextView!
+
+    @IBAction func tapAddButton(sender: UIButton) {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
