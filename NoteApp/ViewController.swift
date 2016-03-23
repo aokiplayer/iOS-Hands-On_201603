beb3f6aa5b62db230d29b3d9d8fd6de74097c1bd
Hiroyuki Aoki <aoki@casareal.co.jp>
2016-03-23 10:52:59 +0900

    テキストフィールドにDelegateを適用
    追加ボタンを押さず、テキストフィールド内でキーボードのReturnを押してもメモを追加できるように変更。

--- NoteApp/ViewController.swift
+++ NoteApp/ViewController.swift
@@ -8,12 +8,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+/// UITextFieldDelegateプロトコルを適用することで、ビューコントローラ自体がdelegateとなる
+class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var textView: UITextView!
 
     /// 追加ボタンがクリックされた時に実行されるメソッド
     @IBAction func tapAddButton(sender: UIButton) {
+        // メモの追加メソッドを呼び出し
+        self.addNewNote()
+    }
+
+    /// 画面が読み込まれ、ビューが用意された後に実行されるメソッド
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // テキストフィールドのdelegateには、ビューコントローラ自身を指定
+        textField.delegate = self
+    }
+
+    /// 入力内容をメモに追加するメソッド
+    func addNewNote() {
         // 入力値を取得（未入力なら何もせずに終了）
         guard let line = textField.text where !line.isEmpty else { return }
         
@@ -30,7 +45,17 @@ class ViewController: UIViewController {
         textField.text = ""
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    // MARK: - UITextFieldDelegate
+
+    /// テキストフィールド内で、Returnキーが押された時に実行されるメソッド
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // 編集を終了するので、カーソルを外しておく
+        textField.resignFirstResponder()
+        return true
+    }
+
+    func textFieldDidEndEditing(textField: UITextField) {
+        // メモの追加メソッドを呼び出し
+        self.addNewNote()
     }
 }
