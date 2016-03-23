f9e09f52cf6c62c7032a2ce583450b680bead71c
Hiroyuki Aoki <aoki@casareal.co.jp>
2016-03-23 10:43:09 +0900

    メモの追加まで実装
    入力値をメモの先頭に追加する処理まで実装済み。キーボード制御や永続化などは未実装。

--- NoteApp/ViewController.swift
+++ NoteApp/ViewController.swift
@@ -12,19 +12,25 @@ class ViewController: UIViewController {
     @IBOutlet weak var textField: UITextField!
     @IBOutlet weak var textView: UITextView!
 
+    /// 追加ボタンがクリックされた時に実行されるメソッド
     @IBAction func tapAddButton(sender: UIButton) {
+        // 入力値を取得（未入力なら何もせずに終了）
+        guard let line = textField.text where !line.isEmpty else { return }
+        
+        // 前回までのメモを取得（Optional型から、!で強制取り出し）
+        let currentNote = textView.text!
+        
+        // メモの先頭に入力値を追加した文字列を作成（間に改行を挟む）
+        let newNote = "\(line)\n\(currentNote)"
+        
+        // テキストビューの表示文字列を、新しいメモで更新
+        textView.text = newNote
+        
+        // テキストフィールドの入力内容をクリア
+        textField.text = ""
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
